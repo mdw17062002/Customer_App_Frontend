c2557289148a190f7c7575dd37cf623f0ae2fe01
Mohini <waghmodemohini911@gmail.com>
2024-07-14 12:21:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import CustomersTable from './CustomersTable';
+import AddCustomer from './AddCustomer';
+import Login from './Login';
+import UpdateCustomer from './UpdateCustomer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/customers" element={<CustomersTable />} />
+          <Route path="/add-customer" element={<AddCustomer />} />
+          {/* <Route path="/update/:uuid" element={<UpdateCustomer />} /> */}
+          <Route path="/update-customer/:uuid" element={<UpdateCustomer />} />
+        </Routes>
       </header>
     </div>
   );
